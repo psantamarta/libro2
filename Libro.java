c353b1e93978a2aaae5b598a591a8fbbe6e2fb62
Patricia <p.santamartamedina@gmail.com>
2020-11-27 12:46:24 +0100

    1847.07 se crea un atributo que indica si el libro es de texto o no y se añade el valor devuelto en getDetalles y en imprimirDetalles

--- Libro.java
+++ Libro.java
@@ -15,41 +15,47 @@ public class Libro {
     private int numeroPaginas;
     private String numeroReferencia;
     private int vecesPrestado;
-    
+    private boolean esLibroDeTexto;
+
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro, int numeroPag)
+    public Libro(String autorLibro, String tituloLibro, int numeroPag, boolean esDeTexto)
     {
         autor = autorLibro;
         titulo = tituloLibro;
         numeroPaginas = numeroPag;
         numeroReferencia = "";
         vecesPrestado = 0;
+        esLibroDeTexto = esDeTexto;
     }
-    
+
     public String getAutor(){
         return autor;
     }
-    
+
     public String getTitulo(){
         return titulo;
     }
-    
+
     public int getNumeroPaginas(){
         return numeroPaginas;
     }
-    
+
     public String getNumeroReferencia(){
         return numeroReferencia;
     }
-   
+
     public int getVecesPrestado(){
         return vecesPrestado;
     }
-    
+
+    public boolean getEsLibroDeTexto(){
+        return esLibroDeTexto;
+    }
+
     public void setNumeroReferencia(String nuevaReferencia){
-        
+
         if (nuevaReferencia.length() > 3){
             numeroReferencia = nuevaReferencia;
         }
@@ -57,36 +63,50 @@ public class Libro {
             System.out.println("Introduzca una referencia con mas de 3 caracteres");
         }
     }
-    
+
     public void imprimeAutor(){
         System.out.println("el autor es " + autor);
     }
-   
+
     public void imprimeTitulo(){
         System.out.println("el titulo es " + titulo);
     }
-    
+
     public void imprimirDetalles(){
+        String esDeTextoONo ="";
+        if (esLibroDeTexto == true){
+            esDeTextoONo ="si";
+        }
+        if(esLibroDeTexto == false){
+            esDeTextoONo ="no";
+        }
         if(numeroReferencia != ""){
-            System.out.println("Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: " + numeroReferencia + ", Veces prestado: " + vecesPrestado);
+            System.out.println("Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: " + numeroReferencia + ", Veces prestado: " + vecesPrestado + ", Es libro de texto: " + esDeTextoONo);
         }
         else{
-            System.out.println("Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: ZZZ" + ", Veces prestado: " + vecesPrestado);
+            System.out.println("Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: ZZZ" + ", Veces prestado: " + vecesPrestado + ", Es libro de texto: " + esDeTextoONo);
         }
-        
+
     }
-    
+
     public String getDetalles(){
         String cadenaADevolver = "";
+        String esDeTextoONo ="";
+        if (esLibroDeTexto == true){
+            esDeTextoONo ="si";
+        }
+        if (esLibroDeTexto == false){
+            esDeTextoONo ="no";
+        }
         if(numeroReferencia != ""){
-            cadenaADevolver = "Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: " + numeroReferencia + ", Veces prestado: " + vecesPrestado;
+            cadenaADevolver = "Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: " + numeroReferencia + ", Veces prestado: " + vecesPrestado + ", Es libro de texto: " + esDeTextoONo;
         }  
         else{
-            cadenaADevolver = "Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: ZZZ" + ", Veces prestado: " + vecesPrestado;
+            cadenaADevolver = "Título: " + titulo + ", Autor: " + autor + ", Páginas: " + numeroPaginas + ", Nº referencia: ZZZ" + ", Veces prestado: " + vecesPrestado + ", Es libro de texto: " + esDeTextoONo;
         }    
         return cadenaADevolver;
     }
-    
+
     public void prestar(){
         vecesPrestado += 1;
     }
